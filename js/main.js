6853c668f3818258d9eecfe2c312e1c23a133504
alex <itmedved@gmail.com>
2013-09-21 17:03:29 +0300

    alert 5

--- js/main.js
+++ js/main.js
@@ -1,27 +1,13 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
-
-    initialize: function() {
         var self = this;
-        this.store = new MemoryStore(function() {
-            self.renderHomeView();
+        var message = $('.search-key').val();
+        this.store.findByName(message, function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+        self.showAlert(message, 'keyup');
         });
-//        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
-    
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -29,17 +15,21 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-
     renderHomeView: function() {
-    var html =
-            "<div class='header'><h1>Home</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key'/>" +
-            "<ul class='employee-list'></ul>" +
-            "</div>"
-    $('body').html(html);
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    this.showAlert('Store Initialized', 'Info');
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+
+
+    initialize: function() {
+
+        var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.store = new MemoryStore(function() {
+            self.renderHomeView();
+        });
     },
 
 };
